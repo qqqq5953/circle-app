a0a446e99b65632ce61b6a0ffdab1ad2ef17f41c
andy.H <qqqq5953@gmail.com>
2022-06-21 10:48:52 +0800

    改成一般 for 迴圈

--- server/actions/getHoldingsTotalInfo.js
+++ server/actions/getHoldingsTotalInfo.js
@@ -3,7 +3,8 @@ const parseFloatByDecimal = require('../tools/parseFloatByDecimal')
 const getHoldingsTotalInfo = (tickers, yesterdayQuote, holdingsTradeInfo) => {
   const holdingsTotalInfo = {}
 
-  tickers.forEach((ticker) => {
+  for (let i = 0; i < tickers.length; i++) {
+    const ticker = tickers[i]
     const stockYesterdayQuote = yesterdayQuote[ticker][0]
     const stockTradeInfo = holdingsTradeInfo[ticker]
 
@@ -27,14 +28,40 @@ const getHoldingsTotalInfo = (tickers, yesterdayQuote, holdingsTradeInfo) => {
       profitOrLossPercentage,
       profitOrLossValue
     }
+  }
 
-    // console.log('stockHistoricalData', stockHistoricalData)
-    // console.log('stockTradeInfo', stockTradeInfo)
-    // console.log('close', typeof close)
-    // console.log('averageCost', typeof averageCost)
-    // console.log('totalShares', typeof totalShares)
-    // console.log('holdingsTotalInfo', holdingsTotalInfo)
-  })
+  // tickers.forEach((ticker) => {
+  //   const stockYesterdayQuote = yesterdayQuote[ticker][0]
+  //   const stockTradeInfo = holdingsTradeInfo[ticker]
+
+  //   const { close } = stockYesterdayQuote
+  //   const { averageCost, totalShares } = stockTradeInfo
+
+  //   const profitOrLossPercentage = parseFloatByDecimal(
+  //     ((close - averageCost) * 100) / close,
+  //     2
+  //   )
+  //   const profitOrLossValue = parseFloatByDecimal(
+  //     (close - averageCost) * totalShares,
+  //     2
+  //   )
+
+  //   stockYesterdayQuote.close = parseFloatByDecimal(close, 2)
+
+  //   holdingsTotalInfo[ticker] = {
+  //     ...stockYesterdayQuote,
+  //     ...stockTradeInfo,
+  //     profitOrLossPercentage,
+  //     profitOrLossValue
+  //   }
+
+  //   // console.log('stockHistoricalData', stockHistoricalData)
+  //   // console.log('stockTradeInfo', stockTradeInfo)
+  //   // console.log('close', typeof close)
+  //   // console.log('averageCost', typeof averageCost)
+  //   // console.log('totalShares', typeof totalShares)
+  //   // console.log('holdingsTotalInfo', holdingsTotalInfo)
+  // })
 
   return holdingsTotalInfo
 }
--- server/actions/getHoldingsTradeInfo.js
+++ server/actions/getHoldingsTradeInfo.js
@@ -3,7 +3,9 @@ const parseFloatByDecimal = require('../tools/parseFloatByDecimal')
 const getHoldingsTradeInfo = (holdings) => {
   let holdingsTradeInfo = {}
 
-  Object.entries(holdings).forEach((stock) => {
+  const holdingsArray = Object.entries(holdings)
+  for (let i = 0; i < holdingsArray.length; i++) {
+    const stock = holdingsArray[i]
     const [ticker, tradeDetails] = stock
     let totalCost = 0
     let totalShares = 0
@@ -19,7 +21,25 @@ const getHoldingsTradeInfo = (holdings) => {
       totalShares,
       averageCost: parseFloatByDecimal(totalCost / totalShares, 2)
     }
-  })
+  }
+
+  // Object.entries(holdings).forEach((stock) => {
+  //   const [ticker, tradeDetails] = stock
+  //   let totalCost = 0
+  //   let totalShares = 0
+
+  //   for (let uid in tradeDetails) {
+  //     const trade = tradeDetails[uid]
+  //     totalCost += parseFloat(trade.cost) * parseFloat(trade.shares)
+  //     totalShares += parseFloat(trade.shares)
+  //   }
+
+  //   holdingsTradeInfo[ticker] = {
+  //     totalCost,
+  //     totalShares,
+  //     averageCost: parseFloatByDecimal(totalCost / totalShares, 2)
+  //   }
+  // })
   return holdingsTradeInfo
 }
 

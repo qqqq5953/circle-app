5afdf6d8ee09c86e9461874f3aaee941bc2b5be7
andy.H <qqqq5953@gmail.com>
2022-06-27 15:33:56 +0800

    add inputSkeleton

---
+++ src/components/skeleton/InputSkeleton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex items-start gap-4 animate-pulse">
+    <div v-for="item in placeholders" :key="item" class="">
+      <input
+        class="
+          form-input
+          block
+          px-4
+          py-6
+          rounded-full
+          bg-gray-300
+          w-full
+          h-8
+          mb-3
+        "
+        :placeholder="item"
+      />
+    </div>
+    <button type="submit" class="form-input rounded-full px-4 py-3 bg-gray-300">
+      add
+    </button>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const placeholders = ref(["ticker", "cost", "shares"]);
+
+    return { placeholders };
+  },
+};
+</script>

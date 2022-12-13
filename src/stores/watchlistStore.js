e841aad894dd86811c2697dbd2855408ebf9511a
andy.H <qqqq5953@gmail.com>
2022-12-13 19:45:49 +0800

    將 watchlist 一些程式碼移到 store

--- src/stores/watchlistStore.js
+++ src/stores/watchlistStore.js
@@ -31,6 +31,75 @@ const useWatchlistStore = defineStore('watchlist', () => {
 
   showCurrentTab(DEFAULT_TAB.value)
 
+  // ---------------------
+  // searchList section
+  const watchlistTableSkeletonContent = ref({
+    tableHead: {
+      hasTableHead: true,
+      th: 'Stocks',
+      td: ['Price', 'Change %', 'Change']
+    },
+    tableBody: {
+      hasTableBody: true,
+      tr: 0,
+      th: 1,
+      td: 3
+    }
+  })
+  const searchList = ref([])
+  const isSearchListLoading = ref(null)
+  const isFocus = ref(null)
+
+  const toggleSearchListSkeleton = (isLoading) => {
+    isSearchListLoading.value = isLoading
+  }
+
+  const toggleSearchList = (focus) => {
+    isFocus.value = focus
+  }
+
+  const getSearchList = (tickerObject) => {
+    switch (tickerObject) {
+      case undefined:
+        searchList.value = undefined // 無搜尋結果
+        break
+      case null:
+        searchList.value = null // 輸入不符格式
+        break
+      default:
+        const { code, ticker } = tickerObject
+        const tempTicker =
+          code !== 'us' || code !== 'mf' ? ticker.split('.')[0] : ticker
+
+        searchList.value = [{ ...tickerObject, tempTicker }]
+    }
+  }
+
+  // watchlist section
+  const isWatchlistLoading = ref(false)
+  const isAddingProcess = ref(false)
+
+  const toggleWatchlistSkeleton = (isLoading) => {
+    isWatchlistLoading.value = isLoading
+  }
+
+  const toggleAddButtonSpinner = (isLoading) => {
+    isAddingProcess.value = isLoading
+  }
+
+  const setSkeletonTableRow = ({ list, rows }) => {
+    if (!list) {
+      watchlistTableSkeletonContent.value.tableBody.tr = rows
+    } else {
+      watchlistTableSkeletonContent.value.tableBody.tr = list.length
+    }
+  }
+
+  const toggleLoadingEffect = (isActivate) => {
+    toggleAddButtonSpinner(isActivate)
+    toggleWatchlistSkeleton(isActivate)
+  }
+
   return {
     DEFAULT_TAB,
     currentTab,

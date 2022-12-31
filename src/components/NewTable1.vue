675b3fb3768aedacce39b74f6c6ebf6d08f95ec6
andy.H <qqqq5953@gmail.com>
2022-12-31 18:04:30 +0800

    調整 Holdings 的UI呈現

--- src/components/NewTable1.vue
+++ src/components/NewTable1.vue
@@ -21,7 +21,6 @@
         border-0
         flex flex-wrap
         items-center
-        hidden
         md:flex
         lg:hidden
       "
@@ -46,13 +45,12 @@
                 pl-6
                 pr-0
                 py-3
-                text-blueGray-500
+                sm:pr-0 sm:pl-4
                 align-middle
-                text-xs
                 uppercase
                 font-semibold
-                text-center
-                sm:text-left
+                text-xs text-center
+                sm:w-1/5
               "
             >
               Stocks
@@ -62,13 +60,13 @@
               class="
                 px-4
                 py-3
-                text-blueGray-500
+                sm:px-0 sm:py-3.5
                 align-middle
                 text-xs
                 uppercase
                 font-semibold
                 text-center
-                sm:text-right
+                sm:w-1/4
               "
             >
               Profit / Loss
@@ -84,7 +82,7 @@
                 uppercase
                 font-semibold
                 text-center
-                sm:text-right
+                sm:w-[13%]
               "
             >
               Price
@@ -100,7 +98,7 @@
                 uppercase
                 font-semibold
                 text-center
-                sm:text-right
+                sm:w-[15%]
               "
             >
               Avg. Cost
@@ -116,17 +114,17 @@
                 uppercase
                 font-semibold
                 text-center
-                sm:text-right
+                sm:w-[13%]
               "
             >
               Shares
             </th>
-            <th class="px-4 py-3 md:hidden lg:block"></th>
+            <th class="px-4 py-3 md:hidden lg:block sm:w-[14%]"></th>
           </tr>
         </thead>
         <tbody>
           <tr
-            class="flex flex-col sm:table-row mx-2 mb-3 rounded"
+            class="flex flex-col sm:table-row mx-2 mb-3 sm:m-0 rounded border"
             v-for="item in holdingsTotalInfo"
             :key="item"
           >
@@ -137,13 +135,26 @@
                 pr-6
                 pt-5
                 pb-0
-                text-xs
-                whitespace-nowrap
-                text-center
-                sm:text-left
+                text-xs text-center
+                sm:pr-0 sm:pl-4 sm:py-3.5 sm:w-1/5
               "
             >
-              <span class="mx-auto">{{ item.ticker }}</span>
+              <div
+                class="
+                  mx-auto
+                  max-w-[85px]
+                  px-1
+                  py-1
+                  shrink-0
+                  rounded-full
+                  text-white text-center
+                  font-semibold
+                  uppercase
+                "
+                :class="item.latestInfo.style"
+              >
+                {{ item.latestInfo.ticker }}
+              </div>
             </th>
             <!-- Profit / Loss -->
             <td
@@ -151,36 +162,52 @@
                 px-4
                 pt-5
                 pb-0
+                sm:px-0 sm:py-3.5
                 border-b border-gray-100
                 sm:border-0
                 text-xs
-                whitespace-nowrap
                 flex
                 items-center
-                sm:table-cell
+                sm:table-cell sm:w-1/4
               "
             >
               <span class="sm:hidden font-semibold mr-2">P / L:</span>
-              <div class="ml-auto text-right">
+              <div class="ml-auto text-right sm:text-center">
                 <span
                   class="inline-block px-2 py-1 rounded"
                   :class="
-                    item.profitOrLossPercentage > 0
+                    item.trade.profitOrLossPercentage > 0
+                      ? 'text-red-600 bg-red-100/70'
+                      : item.trade.profitOrLossPercentage < 0
                       ? 'text-green-700 bg-green-100'
-                      : 'text-red-700 bg-red-100'
+                      : 'text-slate-500 bg-slate-200'
                   "
                 >
-                  <span v-if="item.profitOrLossPercentage !== 0">
+                  <span v-if="item.trade.profitOrLossPercentage !== 0">
                     <i
-                      class="fas fa-arrow-up mr-1 text-green-700"
-                      v-if="item.profitOrLossPercentage > 0"
+                      class="fas fa-arrow-up mr-1 text-red-600"
+                      v-if="item.trade.profitOrLossPercentage > 0"
                     ></i>
-                    <i class="fas fa-arrow-down mr-1 text-red-700" v-else></i>
+                    <i
+                      class="fas fa-arrow-down mr-1 text-green-700"
+                      v-else-if="item.trade.profitOrLossPercentage < 0"
+                    ></i>
                   </span>
-                  {{ item.profitOrLossPercentage }} %
+                  <span v-else>--</span>
+                  {{ item.trade.profitOrLossPercentage }} %
                 </span>
-                <p class="mt-1">
-                  {{ item.profitOrLossValue }}
+                <p
+                  class="mt-1"
+                  :class="
+                    item.trade.profitOrLossPercentage > 0
+                      ? 'text-red-600'
+                      : item.trade.profitOrLossPercentage < 0
+                      ? 'text-green-700'
+                      : 'text-slate-500'
+                  "
+                >
+                  <span class="mr-px">$</span>
+                  <span>{{ item.trade.profitOrLossValue }}</span>
                 </p>
               </div>
             </td>
@@ -190,17 +217,19 @@
                 px-4
                 pt-5
                 pb-0
+                sm:px-0 sm:py-3.5
                 border-b border-gray-100
                 sm:border-0
                 text-xs
-                whitespace-nowrap
                 flex
                 items-center
-                sm:table-cell
+                sm:table-cell sm:w-[13%]
               "
             >
               <span class="sm:hidden font-semibold mr-2">Price:</span>
-              <p class="ml-auto text-right">{{ item.close }}</p>
+              <p class="ml-auto text-right sm:text-center">
+                {{ item.trade.close }}
+              </p>
             </td>
             <!-- Avg. Cost -->
             <td
@@ -208,18 +237,18 @@
                 px-4
                 pt-5
                 pb-0
+                sm:px-0 sm:py-3.5
                 border-b border-gray-100
                 sm:border-0
                 text-xs
-                whitespace-nowrap
                 flex
                 items-center
-                sm:table-cell
+                sm:table-cell sm:w-[15%]
               "
             >
               <span class="sm:hidden font-semibold mr-2">Average Cost:</span>
-              <p class="ml-auto text-right">
-                {{ item.averageCost }}
+              <p class="ml-auto text-right sm:text-center">
+                {{ item.trade.averageCost }}
               </p>
             </td>
             <!-- Shares -->
@@ -228,28 +257,32 @@
                 px-4
                 pt-5
                 pb-0
+                sm:px-0 sm:py-3.5
                 border-b border-gray-100
                 sm:border-0
                 text-xs
-                whitespace-nowrap
                 flex
                 items-center
-                sm:table-cell
+                sm:table-cell sm:w-[13%]
               "
             >
               <span class="sm:hidden font-semibold mr-2">Share:</span>
-              <p class="ml-auto text-right">{{ item.totalShares }}</p>
+              <p class="ml-auto text-right sm:text-center">
+                {{ item.trade.totalShares }}
+              </p>
             </td>
             <td
               class="
                 px-4
                 pt-5
                 pb-0
+                sm:pl-0 sm:py-3.5
+                block
+                sm:table-cell
                 md:hidden
                 lg:block
                 text-xs
-                block
-                sm:table-cell
+                sm:w-[14%]
               "
             >
               <button
@@ -263,7 +296,7 @@
                   ml-auto
                   block
                 "
-                @click="openTradeModal(item.ticker)"
+                @click="openTradeModal(item.trade.ticker)"
               >
                 Trade
               </button>

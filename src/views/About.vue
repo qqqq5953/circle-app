b741dad12ea4e00d4cc51d0a86134489e177f9cb
andy.H <qqqq5953@gmail.com>
2023-04-24 11:18:22 +0800

    新增 about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="text-slate-500">
+    <h2 class="text-black font-semibold text-2xl md:text-4xl md:pb-4">
+      About Circle
+    </h2>
+    <p>
+      Circle is an investment management platform that simplifies the process of
+      managing investments. Our mission is to help people make smarter
+      investment decisions and achieve their financial goals.
+    </p>
+    <p>
+      At Circle, we understand that managing investments can be complex and
+      time-consuming. That's why we've created an intuitive platform that
+      provides users with the tools and information they need to make informed
+      investment decisions.
+    </p>
+    <p>
+      Our platform offers a range of features to help users manage their
+      investments with ease. From creating watchlists to managing portfolios,
+      our platform provides a seamless experience that makes it easy to stay on
+      top of your investments.
+    </p>
+    <p>
+      In addition, Circle provides real-time market information and personalized
+      insights to help users make better investment decisions. Our team of
+      experts is dedicated to helping users achieve their investment goals and
+      is available to provide support whenever needed.
+    </p>
+    <p>
+      At Circle, we believe that everyone should have access to high-quality
+      investment management tools and expertise. That's why we've made our
+      platform affordable and accessible to all.
+    </p>
+    <p>
+      Join the thousands of users who have already discovered the power of
+      Circle's investment management platform. Sign up today and start managing
+      your investments with confidence.
+    </p>
   </div>
 </template>

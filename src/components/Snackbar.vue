9f30e304775e06af29cd028a9d5955ce52725d2f
andy.H <qqqq5953@gmail.com>
2023-05-02 23:33:28 +0800

    調整 snackbar UI

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -1,12 +1,14 @@
 <template>
   <Transition name="bar">
     <div
+      class="fixed right-6 left-6 bottom-6 z-10 mx-auto sm:w-1/2 max-w-[400px] flex flex-col space-y-3"
       v-if="messages.length !== 0"
-      class="fixed right-6 left-6 bottom-6 z-10 sm:mx-auto sm:w-1/2 max-w-[400px] flex flex-col space-y-3"
     >
       <div
-        class="py-3 px-6 relative text-slate-100 bg-indigo-600 text-xs rounded"
-        :class="{ 'border-b border-slate-200': message.errorMessage }"
+        class="py-2 px-6 relative text-slate-700 shadow-md bg-white text-xs rounded-full border"
+        :class="
+          message.status === false ? 'border-red-400' : 'border-green-400'
+        "
         v-for="message in messages"
         :key="message.title"
       >
@@ -23,7 +25,10 @@
           >
             {{ message.title }}
           </div>
-          <div class="ml-auto mr-4 text-xs shrink-0" v-if="message.routeName">
+          <div
+            class="ml-auto mr-4 text-xs shrink-0 rounded"
+            v-if="message.routeName"
+          >
             <router-link
               :to="{
                 name: message.routeName,
@@ -31,19 +36,20 @@
                   tradeResult: JSON.stringify(message),
                 },
               }"
-              class="px-2 py-1.5 rounded text-xs bg-gray-100 text-indigo-700 hover:bg-white"
-              >View</router-link
+              class="px-2 py-1 rounded text-xs hover:bg-gray-100/70"
+              >view</router-link
             >
           </div>
-        </div>
-        <div class="text-xs pt-3 px-6" v-if="message.errorMessage">
-          {{ message.errorMessage }}
+          <button
+            class="ml-auto mr-4 text-xs shrink-0 rounded px-2 py-1 hover:bg-slate-100/70"
+            v-if="message.errorMessage"
+            @click="toAlert(message.errorMessage)"
+          >
+            view
+          </button>
         </div>
         <a
-          class="absolute right-2 grid place-items-center h-6 w-6 cursor-pointer hover:bg-gray-100/30 hover:rounded-full"
-          :class="[
-            message.errorMessage ? 'top-2 mt-px' : 'top-1/2 -translate-y-1/2',
-          ]"
+          class="absolute right-2 grid place-items-center h-6 w-6 cursor-pointer hover:bg-gray-200/30 hover:rounded-full top-1/2 -translate-y-1/2"
           href="#"
           @click.prevent="closeBar(message.id)"
         >
@@ -52,14 +58,35 @@
       </div>
     </div>
   </Transition>
+
+  <BaseAlert v-show="isOpen && alertMsg">
+    <template #header>
+      <h2 class="text-xl lg:text-2xl break-words">Error</h2>
+    </template>
+    <template #body>
+      <p class="text-slate-500 font-light">{{ alertMsg }}</p>
+    </template>
+    <template #footer>
+      <div class="text-right">
+        <button
+          class="text-indigo-600 px-2 py-1.5 mr-2 rounded hover:bg-slate-100 focus:bg-slate-100 focus:outline-none"
+          @click="isOpen = false"
+        >
+          Close
+        </button>
+      </div>
+    </template>
+  </BaseAlert>
 </template>
 
 <script>
 import { ref, watch } from "vue";
 import { storeToRefs } from "pinia";
 import useApiStore from "@/stores/apiStore.js";
+import BaseAlert from "@/components/BaseAlert.vue";
 
 export default {
+  components: { BaseAlert },
   props: {
     barMessage: {
       type: Object,
@@ -69,6 +96,7 @@ export default {
     const messages = ref([]);
     const $store = useApiStore();
     const { axiosMessages } = storeToRefs($store);
+    const isOpen = ref(false);
 
     watch(axiosMessages, (newMessage) => {
       if (newMessage) activateBar(newMessage);
@@ -96,9 +124,18 @@ export default {
       clearTimeout(message.timer);
     }
 
+    const alertMsg = ref(null);
+    function toAlert(msg) {
+      isOpen.value = true;
+      alertMsg.value = msg;
+    }
+
     return {
       messages,
       closeBar,
+      isOpen,
+      toAlert,
+      alertMsg,
     };
   },
 };
